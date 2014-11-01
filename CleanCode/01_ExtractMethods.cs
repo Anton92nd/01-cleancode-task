94228ffe65cce66da3fd7b0bee546b0c37065c34
Anton92nd <anton92nd@gmail.com>
2014-11-01 17:45:29 +0600

    extract methods

--- CleanCode/01_ExtractMethods.cs
+++ CleanCode/01_ExtractMethods.cs
@@ -7,24 +7,18 @@ namespace CleanCode
 	{
 		private static void SaveData(string s, byte[] d)
 		{
-			//open files
-			var fs1 = new FileStream(s, FileMode.OpenOrCreate);
-			var fs2 = new FileStream(Path.ChangeExtension(s, "bkp"), FileMode.OpenOrCreate);
-
-			// write data
-			fs1.Write(d, 0, d.Length);
-			fs2.Write(d, 0, d.Length);
-
-			// close files
-			fs1.Close();
-			fs2.Close();
-
-			// save last-write time
-			string tf = s + ".time";
-			var fs3 = new FileStream(tf, FileMode.OpenOrCreate);
+		    OpenAndWrite(s, d);
+            OpenAndWrite(Path.ChangeExtension(s, "bkp"), d);
+			var tf = s + ".time";
 			var t = BitConverter.GetBytes(DateTime.Now.Ticks);
-			fs3.Write(t, 0, t.Length);
-			fs3.Close();
+		    OpenAndWrite(tf, t);
 		}
+
+	    private static void OpenAndWrite(string s, byte[] d)
+	    {
+	        var fs = new FileStream(s, FileMode.OpenOrCreate);
+	        fs.Write(d, 0, d.Length);
+	        fs.Close();
+	    }
 	}
 }
