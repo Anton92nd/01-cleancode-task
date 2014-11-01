588dd95c467af19b26484ea50f59a55f098b261d
Anton92nd <anton92nd@gmail.com>
2014-11-01 18:17:54 +0600

    02_Chess refactored

--- CleanCode/02_Chess.cs
+++ CleanCode/02_Chess.cs
@@ -1,52 +1,53 @@
 ﻿namespace CleanCode
 {
-	public class Chess
-	{
-		private readonly Board b;
+    public class Chess
+    {
+        private readonly Board _board;
 
-		public Chess(Board b)
-		{
-		this.b = b;
-		}
+        public Chess(Board board)
+        {
+            this._board = board;
+        }
 
-		public string getWhiteStatus() {
-			bool bad=checkForWhite();
-			bool ok=  false;
-			foreach (Loc loc1 in b.Figures(Cell.White))
-			{
-				foreach (Loc loc2 in b.Get(loc1).Figure.Moves(loc1, b)){
-				Cell old_dest = b.PerformMove(loc1, loc2);
-				if (!checkForWhite( ))
-					ok = true;
-				b.PerformUndoMove(loc1, loc2, old_dest);
-				}
-				
-				
-				
-			}
-			if (bad)
-				if (ok)
-					return "check";
-				else return "mate";
-				if (ok)	return "ok";
-			return "stalemate";
-		}
+        public string GetWhiteStatus()
+        {
+            bool checkForWhite = CheckForWhite();
+            bool haveMove = WhiteHaveMove();
+            if (checkForWhite)
+                return haveMove ? "check" : "mate";
+            return haveMove ? "ok" : "stalemate";
+        }
 
-		private bool checkForWhite()
-		{
-			bool bFlag = false;
-			foreach (Loc loc in b.Figures(Cell.Black))
-			{
-				var cell = b.Get(loc);
-				var moves = cell.Figure.Moves(loc, b);
-				foreach (Loc to in moves)
-				{
-					if (b.Get(to).IsWhiteKing)
-						bFlag = true;
-				}
-			}
-			if (bFlag) return true;
-			return false;
-		}
-	}
+        private bool WhiteHaveMove()
+        {
+            bool haveMove = false;
+            foreach (Loc loc in _board.Figures(Cell.White))
+            {
+                foreach (Loc to in _board.Get(loc).Figure.Moves(loc, _board))
+                {
+                    Cell performedMove = _board.PerformMove(loc, to);
+                    if (!CheckForWhite())
+                        haveMove = true;
+                    _board.PerformUndoMove(loc, to, performedMove);
+                }
+            }
+            return haveMove;
+        }
+
+        private bool CheckForWhite()
+        {
+            bool isCheck = false;
+            foreach (Loc loc in _board.Figures(Cell.Black))
+            {
+                var cell = _board.Get(loc);
+                var moves = cell.Figure.Moves(loc, _board);
+                foreach (Loc to in moves)
+                {
+                    if (_board.Get(to).IsWhiteKing)
+                        isCheck = true;
+                }
+            }
+            return isCheck;
+        }
+    }
 }
